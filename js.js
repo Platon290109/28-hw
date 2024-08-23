5e69434b20e789a2c4fef45526900e14760782b9
Platon290109 <platon01.2009@gmail.com>
2024-08-23 20:40:17 +0300

    hw

---
+++ js.js
@@ -0,0 +1,18 @@
+let result = 5 + 5 + '5';
+console.log(result);
+console.log(typeof result)
+
+const email = 'asd@asd.com'
+email.includes("@")
+console.log(email.includes("@"))
+console.log(email.length)
+
+const one = "My, ";
+const two = "name, ";
+const three = "is, ";
+const fullName = one + two + three + "Viktor";
+console.log(fullName);
+
+const userName = 'Platon'
+const payment = 300
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`)
